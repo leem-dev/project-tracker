97e74e4595d5e7ea14e4be39a0527b76fb7e2481
Seleem Alabi <117839317+leem-dev@users.noreply.github.com>
2023-12-29 23:58:41 +0100

    Add files via upload

---
+++ src/component/project-component/project-section-component.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+const ProjectSection = () => {
+  return (
+    <div>
+      <h3>Are you here!!!</h3>
+    </div>
+  );
+};
+
+export default ProjectSection;
