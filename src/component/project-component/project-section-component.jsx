de26243b42b1097364cadc430a307b06d9ca0d09
Seleem Alabi <117839317+leem-dev@users.noreply.github.com>
2023-12-31 01:46:42 +0100

    Update project-section-component.jsx

--- src/component/project-component/project-section-component.jsx
+++ src/component/project-component/project-section-component.jsx
@@ -1,9 +1,49 @@
-import { Box } from "@chakra-ui/react";
+import { Box, VStack, Heading } from "@chakra-ui/react";
+import DateInput from "../date-picker/date-picker-component";
+
+import "./project.styles.css";
 
 const ProjectSection = () => {
+  const contStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  };
   return (
-    <div>
-      <h3>Are you here!!!</h3>
+    <div style={contStyle}>
+      <VStack
+        backgroundColor="gray"
+        w="350px"
+        // p={3}
+        alignItems="center"
+        border="2px dashed red"
+        fontWeight="500"
+      >
+        <Heading
+          as="h1"
+          fontSize="20px"
+          textTransform="uppercase"
+          paddingBottom="2"
+        >
+          Project Tracker
+        </Heading>
+        <Box>
+          <form>
+            <VStack>
+              <label className="projectLabel">
+                Project Name:
+                <input
+                  type="text"
+                  placeholder="project"
+                  className="projectInput"
+                />
+              </label>
+              <DateInput />
+            </VStack>
+          </form>
+        </Box>
+      </VStack>
     </div>
   );
 };
